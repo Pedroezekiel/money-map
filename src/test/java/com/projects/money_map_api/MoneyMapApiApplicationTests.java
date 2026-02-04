f758249ba3230bce0d6dca9bfe10a5ed5e50ff12
pedroezekiel <ezekieleniola36@gmail.com>
2026-02-04 17:44:13 +0100

    Add initial test class for MoneyMapApi application

---
+++ src/test/java/com/projects/money_map_api/MoneyMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.money_map_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
