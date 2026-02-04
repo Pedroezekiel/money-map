b607f20488eb4f1e02a0e29ccc9c32351bc6a4e8
pedroezekiel <ezekieleniola36@gmail.com>
2026-02-04 17:44:33 +0100

    Initialize MoneyMap API application with main class

---
+++ src/main/java/com/projects/money_map_api/MoneyMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.projects.money_map_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMapApiApplication.class, args);
+    }
+
+}
