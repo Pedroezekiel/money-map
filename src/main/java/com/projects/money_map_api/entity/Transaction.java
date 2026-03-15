55ea7aaf2075f41f18dd364a683e5806ccea48b2
pedroezekiel <ezekieleniola36@gmail.com>
2026-03-15 18:06:28 +0100

    Add Transaction entity class with fields and relationships

---
+++ src/main/java/com/projects/money_map_api/entity/Transaction.java
@@ -0,0 +1,38 @@
+package com.projects.money_map_api.entity;
+
+import com.projects.money_map_api.entity.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private BigDecimal amount;
+
+    private TransactionType type;
+
+    private String description;
+
+    private LocalDateTime transactionDate;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateUpdated;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String category;
+}
