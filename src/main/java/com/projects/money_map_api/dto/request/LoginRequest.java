179729a17f4450f7dcfbbda7036e6b5aa5a57f42
pedroezekiel <ezekieleniola36@gmail.com>
2026-04-05 16:27:25 +0100

    Add LoginRequest DTO for user authentication

---
+++ src/main/java/com/projects/money_map_api/dto/request/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.projects.money_map_api.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
