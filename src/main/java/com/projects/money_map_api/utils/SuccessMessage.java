bedd66691fcd94eb077be660ccd55ca7ac5e5688
pedroezekiel <ezekieleniola36@gmail.com>
2026-04-05 16:27:18 +0100

    Add SuccessMessage utility class for standardized success messages

---
+++ src/main/java/com/projects/money_map_api/utils/SuccessMessage.java
@@ -0,0 +1,11 @@
+package com.projects.money_map_api.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SuccessMessage {
+
+    public static final String PASSWORD_RESET_SUCCESS = "Password reset successfully";
+
+}
