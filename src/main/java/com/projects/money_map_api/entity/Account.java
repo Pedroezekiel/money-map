fbe73df9bae8668ccd42a36cbfc0cf5242cdef89
pedroezekiel <ezekieleniola36@gmail.com>
2026-04-05 16:52:07 +0100

    Add Account entity to represent user accounts in the database

---
+++ src/main/java/com/projects/money_map_api/entity/Account.java
@@ -0,0 +1,35 @@
+package com.projects.money_map_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @ManyToOne
+    private User user;
+
+    private String accountName;
+
+    private BigDecimal savingBalance;
+
+    private BigDecimal spendingBalance;
+
+    private String currency;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateUpdated;
+}
