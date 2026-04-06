ad6b77700ff6973410004cdd747c80c5409545eb
pedroezekiel <ezekieleniola36@gmail.com>
2026-04-07 00:27:17 +0100

    Add email validation methods to UserRepository for improved data integrity

--- src/main/java/com/projects/money_map_api/repository/UserRepository.java
+++ src/main/java/com/projects/money_map_api/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.projects.money_map_api.repository;
 
 import com.projects.money_map_api.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByEmail(@Email(message = "Email should be valid") @NotBlank(message = "Email cannot be blank") String email);
+
+    User findByEmail(@Email(message = "Email should be valid") String email);
 }
