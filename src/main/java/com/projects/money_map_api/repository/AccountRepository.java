29bb769cf0ba74e76aa2d61ffb4276f63f0c3b73
pedroezekiel <ezekieleniola36@gmail.com>
2026-04-07 22:58:37 +0100

    Add methods to AccountRepository for account existence check and retrieval by user ID

--- src/main/java/com/projects/money_map_api/repository/AccountRepository.java
+++ src/main/java/com/projects/money_map_api/repository/AccountRepository.java
@@ -1,7 +1,17 @@
 package com.projects.money_map_api.repository;
 
 import com.projects.money_map_api.entity.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    boolean existsAccountByIdAndUserId(String accountId, String userId);
+
+    Optional<Account> findAccountByIdAndUserId(String accountId, String userId);
+
+    Page<Account> findAllByUserId(String id, Pageable pageable);
 }
